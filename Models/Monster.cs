9cf8775563a92d1b4b5597d20c95600fb727542e
Will Harding <atlasblinked@gmail.com>
2017-01-23 10:21:06 +1300

    Add files via upload

---
+++ Models/Monster.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HeroesOfTheColliseumMvc.Models
+{
+    public class Monster
+    {
+        public byte Id { get; set; }
+        public string ImageUrl { get; set; }
+        public string Name { get; set; }
+        public byte Attack { get; set; }
+        public byte Defence { get; set; }
+        public byte CritDam { get; set; }
+        public byte HitPoints { get; set; }
+    }
+}
